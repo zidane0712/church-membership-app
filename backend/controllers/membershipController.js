2ce11b99329ee8d28198b77ad7fa6e7b00dae856
Dustin Aaron Santos <santosdustinaaron07@gmail.com>
2024-07-20 18:30:02 +0800

    add add member

--- backend/controllers/membershipController.js
+++ backend/controllers/membershipController.js
@@ -1,7 +1,16 @@
 // [IMPORTS]
-const Product = require("../models/Membership");
+const Membership = require("../models/Membership");
 
 // [FUNCTIONS]
 module.exports.index = async (req, res) => {
-  res.send("Membership index");
+  const members = await Membership.find({});
+  console.log(members);
+  res.send(members);
+};
+
+module.exports.addMember = async (req, res) => {
+  const member = new Membership(req.body);
+  await member.save();
+  console.log(req.body);
+  res.send("Add member");
 };
--- backend/routes/membershipRoute.js
+++ backend/routes/membershipRoute.js
@@ -7,6 +7,9 @@ const membershipController = require("../controllers/membershipController");
 const asyncHandler = require("../middlewares/asyncHandler");
 
 // [ROUTES]
-router.route("/").get(asyncHandler(membershipController.index));
+router
+  .route("/")
+  .get(asyncHandler(membershipController.index))
+  .post(asyncHandler(membershipController.addMember));
 
 module.exports = router;
